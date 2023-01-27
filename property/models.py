2572045a6d0c5a5b7c548b8644190ec05262f764
Ruk228 <icecreamcrusher21@gmail.com>
2023-01-27 20:06:21 +0400

    update gitignore

--- property/models.py
+++ property/models.py
@@ -65,7 +65,7 @@ class Flat(models.Model):
 class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200)
     apartment = models.ManyToManyField(Flat, verbose_name='Квартир(а\ы) в собственности', related_name="owners", blank=True)
-    
+
     def __str__(self):
         return self.name
 
